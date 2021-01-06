853a5f4cc073f79133f65bb6797f3254deb3a755
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-01-05 21:52:55 -0300

    test: add describe on spec

--- packages/price/src/spec.tsx
+++ packages/price/src/spec.tsx
@@ -2,12 +2,13 @@ import { render, screen } from '@testing-library/react';
 
 import Price from '.';
 
-test('render Price', () => {
-  render(<Price value="199,90" />);
+describe('<Price />', () => {
+  test('should be rendered the component', () => {
+    render(<Price value="199,90" />);
 
-  const price = screen.getByText(/199/i);
-  const priceCents = screen.getByText(/90/i);
-
-  expect(price).toBeInTheDocument();
-  expect(priceCents).toBeInTheDocument();
+    const price = screen.getByText(/199/i);
+    const priceCents = screen.getByText(/90/i);
+    expect(price).toBeInTheDocument();
+    expect(priceCents).toBeInTheDocument();
+  });
 });
