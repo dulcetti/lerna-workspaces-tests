d10c5a99ea5b2d522759a09ac1b0a4e79c0f18e2
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-01-05 21:53:55 -0300

    test: add test for higher prices

--- packages/price/src/spec.tsx
+++ packages/price/src/spec.tsx
@@ -11,4 +11,11 @@ describe('<Price />', () => {
     expect(price).toBeInTheDocument();
     expect(priceCents).toBeInTheDocument();
   });
+
+  test('should be verified if the value is greater than 1000', () => {
+    render(<Price value="1599,90" />);
+
+    const price = screen.getByText(/1.599/i);
+    expect(price).toBeInTheDocument();
+  });
 });
