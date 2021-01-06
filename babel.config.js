259287ce3630983600b86f0243f3477abb7fb8a2
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-01-05 21:49:39 -0300

    chore: add runtime automatic on preset react

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   plugins: ['babel-plugin-styled-components'],
   presets: [
-    '@babel/preset-react',
+    [
+      '@babel/preset-react',
+      {
+        'runtime': 'automatic',
+      },
+    ],
     '@babel/preset-typescript',
     ['@babel/preset-env', { targets: { node: 'current' } }],
   ],
